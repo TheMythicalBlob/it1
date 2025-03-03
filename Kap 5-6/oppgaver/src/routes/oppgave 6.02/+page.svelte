f3b72d660a3cd65644998858f77cb54cb06a35e8
Ali <ali.gondal06@gmail.com>
2025-03-03 14:57:35 +0100

    b

--- Kap 5-6/oppgaver/src/routes/oppgave 6.02/+page.svelte
+++ Kap 5-6/oppgaver/src/routes/oppgave 6.02/+page.svelte
@@ -1,8 +1,11 @@
 <script>
-    let pris_uten_moms = 0
-    let pris_med_moms = 0
+    let pris_uten_moms = 0;
+    let pris_med_moms = 0;
 
     function regn_moms(){
-        
+        pris_med_moms = pris_uten_moms * 1.25; 
     }
 </script>
+
+<input type="pris_med_moms" bind:value={pris_uten_moms} on:input={regn_moms} />
+<p>Pris med moms: {pris_med_moms}</p>
---
+++ Kap 5-6/oppgaver/src/routes/oppgave 6.03/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    let MånedsPris = 175
+    let AntallGB = [1, 5, 15]
+    let DatapakkePris = [79, 149, 249]
+    let brukerForbruk = 0
+    let totalPris = 0
+
+    function beregnPris() {
+        let ekstraData = Math.max(0, brukerForbruk - 1)
+        let prisForEkstraData = 0
+
+        for (let i = AntallGB.length - 1; i >= 0; i--) {
+            while (ekstraData >= AntallGB[i]) {
+                ekstraData -= AntallGB[i]
+                prisForEkstraData += DatapakkePris[i]
+            }
+        }
+
+        totalPris = MånedsPris + prisForEkstraData
+    }
+</script>
+
+<input type="number" bind:value={brukerForbruk} on:input={beregnPris}/>
+<p>Total pris er {totalPris} kr</p>
